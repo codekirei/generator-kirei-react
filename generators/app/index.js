352d6c1b3d0a257f0914df1c59bd864e2d979877
Jacob Blakely <jacob@codekirei.com>
2016-02-19 14:07:31 -0800

    add prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,10 +3,18 @@
 const gen = require('yeoman-generator')
 
 module.exports = gen.Base.extend({
-  foo: function() {
-    console.log('foo')
-  },
-  bar: function() {
-    console.log('bar')
+  prompting: function() {
+    const done = this.async()
+    this.prompt(
+      {
+        type: 'input'
+      , name: 'name'
+      , message: 'What is your project name?'
+      , default: this.appname
+      }
+    , answers => {
+      this.log(answers.name)
+      done()
+    })
   }
 })
